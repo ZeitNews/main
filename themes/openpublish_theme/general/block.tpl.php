ff699d70f9d90761a601df73534895c7742a7b08
JStMartin <jrstmartin@gmail.com>
2012-03-28 18:46:39 -0400

    Preventative maintenance to block.tpl.php for block_class module.

--- themes/openpublish_theme/general/block.tpl.php
+++ themes/openpublish_theme/general/block.tpl.php
@@ -1,7 +1,7 @@
 <?php
 // $Id: block.tpl.php,v 1.1.2.2 2010/12/01 18:06:49 tirdadc Exp $
 ?>
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clearfix block block-<?php print $block->module ?> <?php print block_class($block); ?>">
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clearfix block block-<?php print $block->module ?> <?php if (function_exists(block_class)) print block_class($block); ?>">
 
   <?php if (!empty($block->subject) && $block->module != 'views'): ?>
     <h2><?php print "$block->subject"; ?></h2>
