3f2c908e6d0439f7505e74f3cef96b73f04f1846
JStMartin <jrstmartin@gmail.com>
2012-08-12 17:43:56 -0400

    Closes #64 by @JStMartin: Easier Image Enlargements - New CCK field and main_image theming on content types that use Top Body Image. - New patch to Insert module to facilitate image linking through imagecache. - Fancybox used for image links. Lots of new JS for this functionality. - CSS changes. Hiding br tags in body due to Editor abuse.

--- themes/openpublish_theme/js/openpublish_theme.js
+++ themes/openpublish_theme/js/openpublish_theme.js
@@ -6,30 +6,30 @@ Drupal.behaviors.openpublish = function(context) {
 // Focus cursor into username field for login.
 $('#edit-name').focus();
 
+// Fade out distracting social media icons slightly.
 // http://cssbeauty.com/skillshare/discussion/1593/#Item_5
-$(document).ready(function(){
-  $("#add-this").fadeTo("100", 0.6);
-  $("#add-this").hover(function(){
-    $(this).fadeTo("400", 1.0);
-  },function(){
-    $(this).fadeTo("400", 0.6);
-  });
+$('#add-this').fadeTo('100', 0.6);
+$('#add-this').hover(function(){
+  $(this).fadeTo('400', 1.0);
+},function(){
+  $(this).fadeTo('400', 0.6);
 });
 
+// Set proper margins on images based on float direction.
 // Iterate over all images in #op-content div (body and comments).
-$('#op-content img').each(function(i) {
+$('#op-content img').each(function() {
   
   // If it's likely a user editable image (has style attribute).
-  if ($(this).attr("style")) {
+  if ($(this).attr('style')) {
     // Get the float from the style attribute or also unfortunately the stylesheet.
-    var img_float = $(this).css("float");
+    var img_float = $(this).css('float');
     
     // Change CSS based on which float and if it has a caption.
     // Only downfall here is if you wanted to add custom margins via WYSIWYG, have to use !important.
     // Also if a system img uses a style attribute its margins are going to get clobbered.
-    if (img_float == "left") {
+    if (img_float == 'left') {
       // If it has an image caption.
-      if ($(this).parent().hasClass("image-caption-container")) {
+      if ($(this).parent().hasClass('image-caption-container')) {
         $(this).parent().css({
           'margin': '10px 25px 10px 0px',
           'clear': 'left'
@@ -42,8 +42,8 @@ $('#op-content img').each(function(i) {
       }
     }
   
-    if (img_float == "right") {
-      if ($(this).parent().hasClass("image-caption-container")) {
+    if (img_float == 'right') {
+      if ($(this).parent().hasClass('image-caption-container')) {
         $(this).parent().css({
           'margin': '10px 0px 10px 25px',
           'clear': 'right'
@@ -56,7 +56,91 @@ $('#op-content img').each(function(i) {
       }
     }
   }
+});
 
+// Creates :parents filter. http://stackoverflow.com/a/965962/1055558
+jQuery.expr[':'].parents = function(a,i,m){
+  return jQuery(a).parents(m[3]).length < 1;
+};
+
+// Set fancybox class and rel attributes for images linked to images in .body-content, including main-image.
+$('.body-content img').filter(':parents(.views-slideshow-galleria)').each(function() {
+  // Does the img element have an <a> parent?
+  if ($(this).closest('a').length > 0) {
+    // Does the <a> parent link to an image? If so, add the fancybox class and rel.
+    $(this).closest('a')
+      .filter(function(){
+        return /(jpe?g|png|gif)$/i.test($(this).attr('href'));
+      })
+      .addClass('fancybox').attr('rel', 'gallery')
+    ;
+    // Does the image have a .fancybox class (is it an image link)?
+    if ($(this).closest('a').hasClass('fancybox')) {
+      if ($(this).hasClass('caption')) {
+        // Get title (explicit caption, if exists)
+        var title = $(this).attr('title');
+        // and assign to alt for fancybox use.
+        $(this).attr('alt', title);
+      }
+      // Set informative hover title for fancybox images.
+      $(this).attr('title', 'Click to enlarge.');
+    }
+  }
+});
+
+$('a.fancybox').each(function() {
+  var $a = $(this);
+  // Move the link to surround the img directly on inserted images for caption expander to function.
+  // Cannot include in the function below - kills Fancybox - WTF?!
+  $a.children('.image-caption-container, .image-caption-container-full-width').unwrap().find('img').wrap($a);
+});
+
+/* Fancybox, Version 1.3.4 (2010/11/11) http://fancybox.net. Version 2 requires jQuery 1.6 which I can't do.
+ * Alternate way of calling fancybox() so I can use $(this) and use each anchor's decendent image's alt text.
+ * http://stackoverflow.com/questions/11139067/access-this-inside-fancybox-options
+*/
+$('a.fancybox').each(function() {
+  var $a = $(this);
+
+  // For title and pager for Fancybox titleFormat setting.
+  function formatTitle(title, currentArray, currentIndex, currentOpts) {
+    if (title && title.length) {
+      return '<div id="fancybox-title-over"><strong style="display:block;padding-bottom:5px;line-height:1.4em;">' + title + '</strong>' + (currentArray.length > 1 ? 'Image ' + (currentIndex + 1) + ' of ' + currentArray.length : '') + '</div>';
+    }
+    return (currentArray.length > 1 ? '<div id="fancybox-title-over">Image ' + (currentIndex + 1) + ' of ' + currentArray.length + '</div>' : '');
+  }
+  
+  // Actual use of the fancybox() method.
+  $a.fancybox({
+    'onStart' : function() {
+      // Use the image's alt instead of the default anchor's title.
+      this.title = $a.find('img').attr('alt');
+    },
+    'titlePosition' : 'over',
+    'titleFormat'		: formatTitle,
+    'cyclic'  : true,
+    'padding' : 5,
+    'margin'  : 15,
+    'onComplete'  : function() {
+      $('#fancybox-content, #fancybox-left, #fancybox-right, #fancybox-title').hover(function() {
+        $('#fancybox-title').show();
+      }, function() {
+        $('#fancybox-title').hide();
+      });
+    }
+  });
+});
+
+// jQuery Expander Plugin - http://plugins.learningjquery.com/expander/
+$('div.body-content span.main-image-credit, div.body-content span.caption').expander({
+  slicePoint:       205,
+  expandPrefix:     '... ',
+  expandText:       'more »',
+  beforeExpand: function() {
+    $(this).find('span.details').css({'display' : 'inline'});
+  },
+  userCollapsePrefix: ' ',
+  userCollapseText: '« less'
 });
 
 };
